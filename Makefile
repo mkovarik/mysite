2d1caf4f69130f515ce8354aaf2ab774390866e7
Michael Kovarik <michaelkovarik@outlook.com>
2018-07-08 09:48:35 -0400

    updated makefile to allow for scripted deploy

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+www:
+	./publish.el
 deploy:
 	rsync -a dest/ root@104.236.220.204:/home/www --delete
 clean:
--- publish.el
+++ publish.el
@@ -1,23 +1,24 @@
+#!/usr/bin/emacs --script
 (provide 'publish)
-(with-eval-after-load 'org
-  (require 'ox-publish)
-  (setq org-publish-project-alist
-        '(("org-files"
-           :base-directory "src/"
-           :base-extention "org"
-           :publishing-directory "dest/"
-           :publishing-function org-html-publish-to-html
-           :recursive nil
-           )
-          ("static"
-           :base-directory "src/static/"
-           :base-extension any
-           :recursive t
-           :publishing-directory "dest/static/"
-           :publishing-function org-publish-attachment
-           )
-          ("www"
-           :components ("org-files" "static")
-           )))
 
-  (org-publish "www" t))
+(require 'ox-publish)
+(setq org-publish-project-alist
+    '(("org-files"
+       :base-directory "src/"
+       :base-extention "org"
+       :publishing-directory "dest/"
+       :publishing-function org-html-publish-to-html
+       :recursive nil
+       )
+      ("static"
+       :base-directory "src/static/"
+       :base-extension any
+       :recursive t
+       :publishing-directory "dest/static/"
+       :publishing-function org-publish-attachment
+       )
+      ("www"
+       :components ("org-files" "static")
+       )))
+
+(org-publish "www" t)
